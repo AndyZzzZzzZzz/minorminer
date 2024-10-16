65eb94fdd7ae074035cd5b13eb0b08e4dff3ab6c
Andy Zhang <azhang@dwavesys.com>
2024-10-15 18:37:06 -0700

    Added test framework for raster embedding

---
+++ tests/utils/test_raster_embedding.py
@@ -0,0 +1,61 @@
+# test_raster_embedding.py
+
+import unittest
+import networkx as nx
+import numpy as np
+from utils import raster_embedding_search
+
+class TestRasterEmbedding(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set up common variables for the tests.
+        """
+        pass
+
+    def test_raster_embedding_topology_chimera(self):
+        """
+        Test raster_embedding_search with topology 'chimera'.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+    def test_raster_embedding_topology_pegasus(self):
+        """
+        Test raster_embedding_search with topology 'pegasus'.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+    def test_raster_embedding_topology_zephyr(self):
+        """
+        Test raster_embedding_search with topology 'zephyr'.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+    def test_raster_embedding_invalid_topology(self):
+        """
+        Test raster_embedding_search with an invalid topology.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+    def test_raster_embedding_raster_breadth(self):
+        """
+        Test raster_embedding_search with different raster_breadth values.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+    def test_raster_embedding_greed_depth(self):
+        """
+        Test raster_embedding_search with different greed_depth values.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+
+    def test_search_for_subgraphs_in_subgrid(self):
+        """
+        Test search_for_subgraphs_in_subgrid function.
+        """
+        raise NotImplementedError("Test not implemented yet.")
+
+
+if __name__ == '__main__':
+    unittest.main()
