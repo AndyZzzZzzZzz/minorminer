81abe0fffa49bedb21ec5e9bca1b2541c79463d7
Andy Zhang <129815299+AndyZzzZzzZzz@users.noreply.github.com>
2024-10-25 14:15:16 -0700

    Update minorminer/utils/raster_embedding.py
    Removed documentation for title
    
    Co-authored-by: Kevin Chern <32395608+kevinchern@users.noreply.github.com>

--- minorminer/utils/raster_embedding.py
+++ minorminer/utils/raster_embedding.py
@@ -31,7 +31,6 @@ def visualize_embeddings(H, embeddings=None, title=None, **kwargs):
             embedding is a dictionary mapping nodes of the source graph to 
             nodes in the target graph. If not provided, only the graph `H` 
             will be visualized without specific embeddings.
-        title (str, optional): Title of the plot. Defaults to None.
         **kwargs: Additional keyword arguments passed to the drawing functions 
             (e.g., node size, font size).
     Draws:
